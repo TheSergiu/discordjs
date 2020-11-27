23f2f95e50c94a6926ed2d0b52d3c6f8e87c6a7a
TheSergiu <43616451+TheSergiu@users.noreply.github.com>
2020-11-27 23:43:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -62,12 +62,13 @@ client.on('message', message => {
 		const regexString = /LFG Post: \*\*([0-9]+)\*\* created/;
 		if(message.channel.name === '🔋bot-commands'){//🔋bot-commands
 		
-		const canalID = message.guild.channels.find(channel => channel.name === '🎲organizari');
+		
 		
 		console.log('canalul corect')
 		console.log(message.content);
 			if(message.content.match(regexString)!== null){ //LFG Post: 3487 created.
 			console.log('mesajul corect, scrie in general')
+				const canalID = message.guild.channels.cache.find(channel => channel.name === '🎲organizari');
 				const canal = client.channels.cache.find(channel => channel.name === '📝general')
 				canal.send('@here S-a creat o noua organizare, verificati canalul  <#$canalID.id>')
 			}
