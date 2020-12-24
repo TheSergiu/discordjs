b98876468e10a40bc200741f1b0c63b1a4613b5b
TheSergiu <43616451+TheSergiu@users.noreply.github.com>
2020-12-24 15:30:02 +0200

    Update lfgNotify.ts

--- src/modules/lfgNotify.ts
+++ src/modules/lfgNotify.ts
@@ -12,7 +12,7 @@ export class LfgNotify {
     if (message.channel.type !== 'text') return;
     if (!message.author.bot) return;
 
-    if (message.channel.name === '🚨lfg-events🚨') {
+    if (message.channel.name === '📝lfg_create📝') {
 
       //LFG Post: 3487 created.
       if (/LFG Post: \*\*([0-9]+)\*\* created/i.test(message.content)) {
